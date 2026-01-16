be72081695177215bdbeb1eb639e210cdef7bde9
TU NOMBRE <tuemail@gmail.com>
2026-01-16 14:52:29 -0600

    Practicas de Python avanzado

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,8 +1,10 @@
 # Prueba de Python
 # Se añadió la cantidad 5 en la variable c
+# Nueva variable E
 a = 10 
 b = 30
-c = 30 * 10 * 5
+e = 1.1
+c = 30 * 10 * 5 / e
 print("Resultado=",c)
 
 # Cambio a factor 200 por Directorio 
